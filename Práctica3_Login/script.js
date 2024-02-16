d123163c9dfc73a7288329f72652686afd96dbdd
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-02-15 22:53:29 -0600

    práctica 3: Mensaje de login y registro

---
+++ Práctica3_Login/script.js
@@ -0,0 +1,13 @@
+function login() {
+    var usuario = document.getElementById("usuario").value;
+    var contraseña = document.getElementById("contraseña").value;
+    console.log("Login correcto " + usuario);
+    alert("Login correcto " + usuario);
+}
+
+function registro() {
+    var usuario = document.getElementById("usuario").value;
+    console.log("Registro con éxito " + usuario);
+    alert("Registro con éxito " + usuario);
+}
+
