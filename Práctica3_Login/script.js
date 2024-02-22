8d26361c07254365dff3beb5533e1f2bc7e942a6
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-02-21 23:05:19 -0600

    Práctica 4

--- Práctica3_Login/script.js
+++ Práctica3_Login/script.js
@@ -1,13 +1,28 @@
-function login() {
-    var usuario = document.getElementById("usuario").value;
-    var contraseña = document.getElementById("contraseña").value;
-    console.log("Login correcto " + usuario);
-    alert("Login correcto " + usuario);
-}
+var articulo = document.getElementById(gastoInput);
+var monto = document.getElementById(montoInput);
+var total = 0;
+var seccionTicket = document.getElementById("Ticket");
+var seccionTotal = document.getElementById("Ticket");
 
-function registro() {
-    var usuario = document.getElementById("usuario").value;
-    console.log("Registro con éxito " + usuario);
-    alert("Registro con éxito " + usuario);
+function guardar() {
+    articulo = gastoInput.value;
+
+    //parseInt convierte de string a número
+    monto = parseInt(montoInput.value);
+    total = total + monto;
+
+    //Crear elemento en html
+    let elementoTicket = document.createElement("p");
+    elementoTicket.innerHTML = articulo + "....... $" + monto;
+    let elementoTotal = document.createElement("p");
+    elementoTotal.innerHTML = "Total: $" + total;
+
+    //Insertar elemento en html
+    seccionTicket.appendChild(elementoTicket);
+    seccionTotal.appendChild(elementoTotal);
+
+    //Limpiar el input
+    montoInput.value = "";
+    gastoInput.value = "";
 }
 
