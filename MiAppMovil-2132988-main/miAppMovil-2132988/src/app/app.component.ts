5cfed3ef7d8f771c79c0deba9c906394dfc258ee
RebecaJara <rebeca.jaramilloc@uanl.edu.mx>
2024-04-30 17:21:15 -0600

    Práctica 9: Servicios en Angular

--- MiAppMovil-2132988-main/miAppMovil-2132988/src/app/app.component.ts
+++ MiAppMovil-2132988-main/miAppMovil-2132988/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component} from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 import { CommonModule } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, CommonModule],
+  imports: [IonApp, IonRouterOutlet, CommonModule, HttpClientModule],
 })
 export class AppComponent {
   constructor() {}
--- MiAppMovil-2132988-main/miAppMovil-2132988/src/app/home/home.page.ts
+++ MiAppMovil-2132988-main/miAppMovil-2132988/src/app/home/home.page.ts
@@ -3,13 +3,14 @@ import { IonicModule } from '@ionic/angular';
 
 import { CommonModule } from '@angular/common';
 import { ListaAlumnosComponent } from '../lista-alumnos/lista-alumnos.component';
+import { ProductosComponent } from '../productos/productos.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, ListaAlumnosComponent],
+  imports: [IonicModule, CommonModule, ListaAlumnosComponent, ProductosComponent],
 })
 export class HomePage {
   constructor() { }
